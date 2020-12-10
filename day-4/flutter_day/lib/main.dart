3f266dbe0a16f7c9d411c4bf34ee10b19b730003
Jamilu Salisu <Jamilusalis@gmail.com>
2020-12-10 21:27:57 +0100

    kickstart training

---
+++ day-4/flutter_day/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          title: Text("Demo: Home Page"),
+          backgroundColor: Colors.black87,
+        ),
+        body: Image.asset('image/luffy.png'))));
